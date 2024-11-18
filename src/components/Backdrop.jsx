501d167aa7d72843892f1a3ff7ed8392705c3c8a
agartanah <vyatin2017@mail.ru>
2024-11-18 19:59:46 +0700

    Backdrop with redux

--- src/components/Backdrop.jsx
+++ src/components/Backdrop.jsx
@@ -1,7 +1,7 @@
-import { useAppContext } from "../contexts/TaskManagerContext";
+import { useSelector } from "react-redux";
 
 export default function Backdrop() {
-    const { currOperation } = useAppContext();
+    const currOperation = useSelector((state) => state.tasks.currOperation);
     
     return (
         <div className={ currOperation !== '' ? "backdrop backdrop-open" : "backdrop" }></div>
