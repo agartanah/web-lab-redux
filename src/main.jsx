d81338e9de967a710623bf92d84d5bdbbeeb904b
agartanah <vyatin2017@mail.ru>
2024-11-18 19:54:16 +0700

    add Provider with store in render

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { Provider } from './contexts/TaskManagerContext.jsx';
+import store from './redux/store.js';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
-  <Provider>
-    <App />
+  <Provider store={ store }>
+    <App/>
   </Provider>
 );
